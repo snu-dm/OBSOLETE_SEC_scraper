c831d65a2249d79720442a29fc06ee02a0ac51d8
Jongwon Lee <59178066+jwjwjwlee@users.noreply.github.com>
2024-02-01 16:24:17 +0900

    parser update

--- parser/parser2.py
+++ parser/parser2.py
@@ -1,3 +1,15 @@
+'''
+RETURN TYPE
+X organization sec
+O name(ticker) tsla
+O year 2021
+X filing_type 10-K
+X item_type 1
+O documentdate
+
++ 추가로 파일명(html에서부터)
+'''
+
 import os
 import numpy as np
 import glob
@@ -7,20 +19,37 @@ from bs4 import BeautifulSoup
 import re, unidecode
 from collections import OrderedDict
 import json
+from datetime import datetime
 
 # Initial setup - currently set as a temporary file path in the current directory
 current_path = os.path.dirname(__file__)
 experiment_path = os.path.join(current_path, 'experiment_files')
 
 df = pd.DataFrame(columns=[['ticker','text_length']])
-p_item1_start = re.compile('(item)\s+[1I]\s*[.:-]*\s*\w{0,5}\s*(business)', re.I)
-p_item2_start = re.compile('(item)\s+[2I]+\s*[.:-]*\s*\w*\s*\w*\s*(properties|communitiesn)', re.I)
-table_pattern = re.compile('\s+[0-9]+\s+(table of contents)\s+', re.I) # table of contents 제거
-
 file_paths = glob.glob('{}/*'.format(experiment_path), recursive=True)
 print("Total number of files: " + str(len(file_paths)))
 
+'''
+def dic_concac(d):
+    t = ''
+    for key in d:
+        t += key
+        t += '\n'
+        
+        if len(d[key]) > 0:
+            for key2 in d[key]:
+                t += d[key][key2]
+                t += '\n'
+
+                if len[d[key][key2]] > 0:
+                    for i in d[key][key2]:
+                        t += d[key][key2][i]
+                        t += '\n'
+
+        t += '\n'
+'''
 for i in range(1): #range(len(file_paths)):
+    
     print(str(i)+" processing started")
 
     infos = file_paths[i].split('\\')[-1].split('_')
@@ -32,81 +61,76 @@ for i in range(1): #range(len(file_paths)):
         soup = BeautifulSoup(f.read(), 'lxml')
         text = unidecode.unidecode(soup.get_text('\n'))
 
-        lines_with_tags = []
-
-
-        for tag in soup.find_all(True):
-            if tag.name:
-                tag_name = unidecode.unidecode(tag.name)
-                tag_text = unidecode.unidecode(tag.get_text(strip=True))
-
-                if tag.attrs:
-                    tag_name += " " + " ".join([f'{key}="{value}"' for key, value in tag.attrs.items()])
-
-            # Check if tag_text is non-empty before appending to the list
-            if tag_text:
-                line = (tag_name, tag_text)
-                lines_with_tags.append(line)
-        
-        item_1 = 'ITEM 1. BUSINESS'
-        item_1a = 'ITEM 1A. RISK FACTORS'
-
-        start_1 = 0
-        start_1a = -1
-        overview_idx = -1
-
-
-        for i in range(len(lines_with_tags)):
-            if item_1 in lines_with_tags[i][1]:
-                start_1 = i
-            #if item_1a in lines_with_tags[i][1]:
-            #    start_1a = i
-            if ('Overview' in lines_with_tags[i][1]) or ('OVERVIEW' in lines_with_tags[i][1]):
-                overview_idx = i
-        
-        if start_1 == 0:
-            print("PARSING ERROR, NO ITEM 1")
-
-        #if start_1a == -1:
-        #    od.pop()
-
-
-        tag_bt = lines_with_tags[start_1][0]
-        tag_st = lines_with_tags[overview_idx][0]
-        tag_mt = lines_with_tags[overview_idx+1][0]
-        
-        num_lines_test = 600
-
-        given_data = lines_with_tags[start_1:start_1+num_lines_test]
-
         od = OrderedDict()
 
-        prev_tag = ''
-        for i in range(len(given_data)):
-            data_tag = given_data[i][0]
-            data_text = given_data[i][1]
-            
+        item_1 = 'ITEM 1. BUSINESS'
+        item_1a = 'ITEM 1A. RISK FACTORS'
+        item_1b = 'ITEM 1B. UNRESOLVED STAFF COMMENTS'
+        item_2 = 'ITEM 2. PROPERTIES'
 
-            if data_tag == tag_bt:
-                od[data_text] = OrderedDict()
-            if data_tag == tag_st:
-                od[next(reversed(od))][data_text] = []
-            if data_tag == tag_mt:
-                od[next(reversed(od))][next(reversed(od[next(reversed(od))]))].append(data_text)
-
-            prev_tag = data_tag
+        def is_end(text):
+            return (item_1b in text) or (item_2 in text)
         
+        def is_bt(text):
+            return (item_1 in text) or (item_1a in text)
 
-        document = json.dumps(od, indent=4)
+        st_tag = ''
+        def is_st(text, name):
+            global st_tag
+            if st_tag == '' and ('Overview' in text) or ('OVERVIEW' in text):
+                st_tag = name
+                return True
+            else:
+                return name == st_tag and len(text) > 1
+        
+        mt_tag = ''
+        prev = ('', '')
+        def is_mt(text, name, prev):
+            global mt_tag
+            if mt_tag == '' and is_st(prev[0], prev[1]):
+                mt_tag = name
+                return True
+            else:
+                return name == mt_tag and len(text) > 1
+
+        for tag in soup.find_all('span'):
+            print(od)
+            if tag.name and tag.attrs:
+                tag_text = unidecode.unidecode(tag.get_text(strip=True))
+                tag_name = " ".join([f'{key}="{value}"' for key, value in tag.attrs.items()])
+                
+                print(tag_text, tag_name)
+
+                if is_end(tag_text):
+                    break
+
+                if is_bt(tag_text):
+                    print('33')
+                    if len(od) == 0:
+                        od[item_1] = OrderedDict()
+                    else:
+                        od[item_1a] = OrderedDict()
+                elif len(od) > 0 and is_st(tag_text, tag_name): # and len(od) > 0:
+                    if len(od) == 1:
+                        od[item_1][tag_text] = []
+                    elif len(od) == 2:
+                        od[item_1a][tag_text] = []
+                    if prev == ('', ''):
+                        prev = (tag_text, tag_name)
+                elif len(od) > 0 and is_mt(tag_text, tag_name, prev): # and len(od) > 0:
+                    od[next(reversed(od))][next(reversed(od[next(reversed(od))]))].append(tag_text)          
+
+        print(st_tag, mt_tag)
+
+        document = json.dumps(od, indent=8)
+        
+        # Saving the log - for experiments, need change when saving to database
+        current_time = datetime.now().time()
+        file_name = f"log_parser2_{current_time}.txt"
+
+        with open(file_name, 'w') as log:
+            log.write(document)
 
     except Exception as e:
         df.loc[len(df)] = [infos[0], 0]
-        print("error : %s, Missed File : %s, Missed File URL: %s" % (e, infos[:1], file_paths[i]))
-
-
-    # Saving the log - for experiments, need change when saving to database
-
-    file_name = f"log3_{i}.txt"
-
-    with open(file_name, 'w') as log:
-        log.write(document)
+        print("error : %s, Missed File URL: %s" % (e, file_paths[i]))
