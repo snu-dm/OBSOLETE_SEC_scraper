40313925f27d99534424e4723768af21ab57b2e9
yoondor <5yoondori@snu.ac.kr>
2024-02-01 14:32:16 +0900

    Refactoring for Modularity

--- sec_crawl.py
+++ sec_crawl.py
@@ -2,13 +2,12 @@ from sec_utils import *
 import pandas as pd
 import argparse
 
-def main(args):
+def crawl(args):
     seccrawler = SecCrawler()
     # seccrawler.filing_10K('MSFT','0000789019','20210719','2000000')
     dataList = seccrawler.filing_10K(args.ticker, args.cik,'20240127', 10)
 
     print(f"num: {len(dataList)}")
-    print(dataList[0][:1500])
 
     return dataList
 
@@ -18,4 +17,4 @@ if __name__=='__main__':
     parser.add_argument('cik', type=str)
     args = parser.parse_args()
 
-    main(args)
+    crawl(args)
